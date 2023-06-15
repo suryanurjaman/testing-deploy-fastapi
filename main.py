2b397b2a4c7fe460c2391ad0dd679b074d0f4621
Surya <suryanurjaman91@gmail.com>
2023-06-15 14:49:22 +0700

    testing deploy 5

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ async def greeting():
   
 # run the app on defined host and port
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=3000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
